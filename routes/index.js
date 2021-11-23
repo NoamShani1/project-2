cf3f6c120229b416de765909b4c76a1f5aac519f
John Doe <adamalfraih@gmail.com>
2021-11-23 11:07:06 +0100

    added passport feature to sign up/login with github. added an avatar to the user model. added /profile

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,35 @@
 const router = require("express").Router();
 
+// Passport & Profile hbs
+const loginCheck = () => {
+  return (req, res, next) => {
+    // with basic-auth: req.session.user
+    if (req.isAuthenticated()) {
+      next()
+    } else {
+      res.redirect('/login')
+    }
+  }
+}
+
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+
+
+
+
+
+
+
+
+router.get('/profile', loginCheck(), (req, res, next) => {
+  // with basic-auth: req.session.user
+  const loggedInUser = req.user
+  res.render('profile', { user: loggedInUser })
 });
 
 module.exports = router;
